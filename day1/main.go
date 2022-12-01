f8703c20920ef2b30dfc9026fb902c66bfd84b0f
Christian Muirhead <xtian@babbageclunk.com>
2022-12-01 21:10:07 +1300

    Day 1

---
+++ day1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("day1.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+	var vals []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" && total != 0 {
+			vals = append(vals, total)
+			total = 0
+			continue
+		}
+		val, err := strconv.Atoi(line)
+		check(err)
+		total += val
+	}
+
+	sort.Ints(vals)
+	fmt.Println("highest", vals[len(vals)-1])
+	sum := 0
+	for _, v := range vals[len(vals)-3:] {
+		sum += v
+	}
+	fmt.Println("top 3 sum", sum)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
